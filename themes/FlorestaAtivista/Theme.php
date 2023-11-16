410f28bc79bedf29c608cb6fb77d88ab6ab9bb40
erleibiazzio <erleibiazzio@gmail.com>
2023-11-16 17:30:00 -0300

    Cria thema padrão do ambiente Floresta ativista

---
+++ themes/FlorestaAtivista/Theme.php
@@ -0,0 +1,30 @@
+<?php
+namespace FlorestaAtivista;
+
+use MapasCulturais\App;
+
+
+// class Theme extends \Subsite\Theme {
+class Theme extends \MapasCulturais\Themes\BaseV2\Theme {
+ 
+    static function getThemeFolder() {
+        return __DIR__;
+    }
+
+    function _init() {
+        parent::_init();
+
+        $app = App::i();
+
+         // Manifest do five icon
+        $app->hook('GET(site.webmanifest)', function() use ($app) {
+            /** @var \MapasCulturais\Controller $this */
+            $this->json([
+                'icons' => [
+                    [ 'src' => $app->view->asset('img/favicon-192x192.png', false), 'type' => 'image/png', 'sizes' => '192x192' ],
+                    [ 'src' => $app->view->asset('img/favicon-515x515.png', false), 'type' => 'image/png', 'sizes' => '512x512' ],
+                ],
+            ]);
+        });
+    }
+}
