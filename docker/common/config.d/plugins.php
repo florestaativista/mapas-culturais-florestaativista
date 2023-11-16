047f23eb8c3696dc051074954a8bf0582536a532
erleibiazzio <erleibiazzio@gmail.com>
2023-11-16 17:43:15 -0300

    coinfigura MapasBlame no ambiente

--- docker/common/config.d/plugins.php
+++ docker/common/config.d/plugins.php
@@ -2,6 +2,14 @@
 
 return [
     'plugins' => [
+        'MapasBlame' => [
+            'namespace' => 'MapasBlame',
+            'config' => [
+                'request.logData.PATCH' => function ($data) {
+                    return $data;
+                },
+            ]
+        ],
         'AdminLoginAsUser' => ['namespace' => 'AdminLoginAsUser'],
         'MultipleLocalAuth' => [ 'namespace' => 'MultipleLocalAuth' ],
         'SamplePlugin' => ['namespace' => 'SamplePlugin'],
