e12fa62d527392e6b4b2de3a4fd87060fca98ab6
erleibiazzio <erleibiazzio@gmail.com>
2023-11-22 17:50:03 -0300

    Configura o OpauthAuthentik como autenticador padrão do ambiente

--- docker/production/config.d/authentication.php
+++ docker/production/config.d/authentication.php
@@ -3,38 +3,17 @@
 $prot_part = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] ? 'https://' : 'http://';
 //added @ for HTTP_HOST undefined in Tests
 $host_part = @$_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']);
-if(substr($host_part,-1) !== '/') $host_part .= '/';
+if (substr($host_part, -1) !== '/') $host_part .= '/';
 $_APP_BASE_URL = $prot_part . $host_part;
 
 return [
-    'auth.provider' => '\MultipleLocalAuth\Provider',
-    'auth.config' => array(
-        'salt' => env('AUTH_SALT', null),
+    'auth.provider' => 'MapasCulturais\AuthProviders\OpauthAuthentik',
+    'auth.config' => [
+        'salt' => env('AUTH_SALT', 'SECURITY_SALT'),
         'timeout' => '24 hours',
-        'strategies' => [
-            'Facebook' => array(
-               'app_id' => env('AUTH_FACEBOOK_APP_ID', null),
-               'app_secret' => env('AUTH_FACEBOOK_APP_SECRET', null),
-               'scope' => env('AUTH_FACEBOOK_SCOPE', 'email'),
-            ),
-
-            'LinkedIn' => array(
-                'api_key' => env('AUTH_LINKEDIN_API_KEY', null),
-                'secret_key' => env('AUTH_LINKEDIN_SECRET_KEY', null),
-                'redirect_uri' => $_APP_BASE_URL . 'autenticacao/linkedin/oauth2callback',
-                'scope' => env('AUTH_LINKEDIN_SCOPE', 'r_emailaddress')
-            ),
-            'Google' => array(
-                'client_id' => env('AUTH_GOOGLE_CLIENT_ID', null),
-                'client_secret' => env('AUTH_GOOGLE_CLIENT_SECRET', null),
-                'redirect_uri' => $_APP_BASE_URL . 'autenticacao/google/oauth2callback',
-                'scope' => env('AUTH_GOOGLE_SCOPE', 'email'),
-            ),
-            'Twitter' => array(
-                'app_id' => env('AUTH_TWITTER_APP_ID', null),
-                'app_secret' => env('AUTH_TWITTER_APP_SECRET', null),
-            ),
-
-        ]
-    ),
+        'client_id' => env('AUTH_AUTHENTIK_APP_ID', 'sunTd7lB77Qlxer4mhehbkcpVd0TdoL6mthqB7Av'),
+        'client_secret' => env('AUTH_AUTHENTIK_APP_SECRET', '8EV1lx3SoWqNpOXQUXPkQTXwlT4DniYCgUh7exHhKhNoD9dcltbplAYtkhDfPW9Mpl3DOsNv1wtoc3JQdk8N5IsWXM4SC6vUdIKz6qFUvxmvC2FKi24JPqMnuZzXjcY2'),
+        'scope' => env('AUTH_AUTHENTIK_SCOPE', 'openid profile email'),
+        'login_url' => env('AUTH_AUTHENTIK_LOGIN_URL', 'https://dev-id.florestaativista.org'),
+    ]
 ];
