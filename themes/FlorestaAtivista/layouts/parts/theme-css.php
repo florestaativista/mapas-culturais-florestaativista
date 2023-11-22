f508b6777dacfef820d30a4e74e5b935cfeff0c2
Thiago Monteiro <thiagomonteiro.geo@gmail.com>
2023-11-22 16:22:08 -0300

    css home and footer

--- themes/FlorestaAtivista/layouts/parts/theme-css.php
+++ themes/FlorestaAtivista/layouts/parts/theme-css.php
@@ -5,4 +5,11 @@
         background-position: center;
         background-size: cover;
     }
+    .home-developers__content .dev-icon{
+        background-image: url(<?php $this->asset('img/dev-star.png') ?>);
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;                
+    }    
+
 </style>
