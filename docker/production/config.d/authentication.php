2dbc0f4049f7ce041d9f1af488bac32caab21551
erleibiazzio <erleibiazzio@gmail.com>
2023-11-23 16:40:17 -0300

    Insere imagem padrão de e-mail

--- docker/production/config.d/authentication.php
+++ docker/production/config.d/authentication.php
@@ -11,9 +11,9 @@ return [
     'auth.config' => [
         'salt' => env('AUTH_SALT', 'SECURITY_SALT'),
         'timeout' => '24 hours',
-        'client_id' => env('AUTH_AUTHENTIK_APP_ID', 'sunTd7lB77Qlxer4mhehbkcpVd0TdoL6mthqB7Av'),
-        'client_secret' => env('AUTH_AUTHENTIK_APP_SECRET', '8EV1lx3SoWqNpOXQUXPkQTXwlT4DniYCgUh7exHhKhNoD9dcltbplAYtkhDfPW9Mpl3DOsNv1wtoc3JQdk8N5IsWXM4SC6vUdIKz6qFUvxmvC2FKi24JPqMnuZzXjcY2'),
-        'scope' => env('AUTH_AUTHENTIK_SCOPE', 'openid profile email'),
-        'login_url' => env('AUTH_AUTHENTIK_LOGIN_URL', 'https://dev-id.florestaativista.org'),
+        'client_id' => env('AUTH_AUTHENTIK_APP_ID', ''),
+        'client_secret' => env('AUTH_AUTHENTIK_APP_SECRET', ''),
+        'scope' => env('AUTH_AUTHENTIK_SCOPE', ''),
+        'login_url' => env('AUTH_AUTHENTIK_LOGIN_URL', ''),
     ]
 ];
