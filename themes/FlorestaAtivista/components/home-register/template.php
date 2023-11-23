c28bddd498cc7609e271f6eb20768918e1599438
erleibiazzio <erleibiazzio@gmail.com>
2023-11-23 16:47:15 -0300

    Evita que o botão de cadastrar por na sessão register seja exibido para usuários autenticados

--- themes/FlorestaAtivista/components/home-register/template.php
+++ themes/FlorestaAtivista/components/home-register/template.php
@@ -16,7 +16,7 @@ use MapasCulturais\i;
         <label class="home-register__content--title"><?= $this->text('title', i::__('Faça seu cadastro e colabore com o Mapas Culturais')) ?></label>
         <p class="home-register__content--description"><?= $this->text('description', i::__('Colabore com a plataforma livre, colaborativa e interativa de mapeamento do cenário cultural e instrumento de governança digital no aprimoramento da gestão pública, dos mecanismos de participação e da democratização do acesso às políticas culturais promovidas pela Secretaria da Cultura.')); ?>
         </p>
-        <a href="<?= $app->createUrl('autenticacao', 'register') ?>" class="home-register__content--button button button--icon button--bg">
+        <a v-if="global.auth.isGuest" href="<?= $app->createUrl('auth') ?>" class="home-register__content--button button button--icon button--bg">
             <?= i::__('Fazer Cadastro')?>
             <mc-icon name="access"></mc-icon>
         </a>
