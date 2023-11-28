50b1f0c8d467263cc94b9c5932e7b4c4fcbec31d
erleibiazzio <erleibiazzio@gmail.com>
2023-11-28 12:22:46 -0300

    Configura ambiente para ocultar os ids das entidades

---
+++ docker/common/config.d/show.entityIds.php
@@ -0,0 +1,13 @@
+<?php 
+
+ return[
+    'app.showEntityIds' => [
+      "agent" => env("SHOW_ENTITY_IDS_AGENT",false),
+      "event" => env("SHOW_ENTITY_IDS_EVENT",false),
+      "space" => env("SHOW_ENTITY_IDS_SPACE",false),
+      "opportunity" => env("SHOW_ENTITY_IDS_OPPORTUNITY",false),
+      "project" => env("SHOW_ENTITY_IDS_PROJECT",false),
+      "user" => env("SHOW_ENTITY_IDS_PROJECT",false),
+    ],
+ ];
+    
