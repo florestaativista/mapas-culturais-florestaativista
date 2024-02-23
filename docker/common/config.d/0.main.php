5376fa3cdcd351f6bcd72758504d0717b4a23806
erleibiazzio <erleibiazzio@gmail.com>
2024-02-23 08:51:38 -0300

    Atualiza ao texto de descrição do site

--- docker/common/config.d/0.main.php
+++ docker/common/config.d/0.main.php
@@ -2,7 +2,7 @@
 
 return [
     'app.siteName' => 'Mapas Floresta Ativista',
-    'app.siteDescription' => 'O Mapas Culturais é uma plataforma colaborativa que reúne informações sobre agentes, espaços, eventos, projetos culturais e oportunidades',
+    'app.siteDescription' => 'A Floresta Ativista é uma plataforma que reúne um ecossistema ativista de coletivos, redes, iniciativas e pessoas com vontade de construir e estar em comunidades',
     
     // Define o tema ativo no site principal. Deve ser informado o namespace do tema e neste deve existir uma classe Theme.
     'themes.active' => 'FlorestaAtivista',
