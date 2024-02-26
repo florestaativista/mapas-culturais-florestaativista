80f1475b98bdb62be69510a29935e18955d5cf52
wolegario49@gmail.com <wolegario49@gmail.com>
2024-02-26 16:24:33 -0300

    cria o  footer para pode exibir as logos Ref.: #16

---
+++ themes/FlorestaAtivista/layouts/parts/logo-footer.php
@@ -0,0 +1,27 @@
+<div class="logo-footer">
+    <img src="<?php $this->asset('img/footer/casa-das-redes.png'); ?>">
+    <img src="<?php $this->asset('img/footer/casa-ninja-amazonia.png'); ?>">
+    <img src="<?php $this->asset('img/footer/casa-ninja-bahia.png'); ?>">
+    <img src="<?php $this->asset('img/footer/casa-ninja-tiradentes.png'); ?>">
+    <img src="<?php $this->asset('img/footer/cine-ninja.png'); ?>">
+    <img src="<?php $this->asset('img/footer/design-ativista.png'); ?>">
+    <img src="<?php $this->asset('img/footer/ella.png'); ?>">
+    <img src="<?php $this->asset('img/footer/emergentes.png'); ?>">
+    <img src="<?php $this->asset('img/footer/esudantes-ninja.png'); ?>">
+    <img src="<?php $this->asset('img/footer/foda.png'); ?>">
+    <img src="<?php $this->asset('img/footer/fora-do-eixo.png'); ?>">
+    <img src="<?php $this->asset('img/footer/midia-ninja-1.png'); ?>">
+    <img src="<?php $this->asset('img/footer/midia-ninja-2.png'); ?>">
+    <img src="<?php $this->asset('img/footer/minas-ninja.png'); ?>">
+    <img src="<?php $this->asset('img/footer/nave.png'); ?>">
+    <img src="<?php $this->asset('img/footer/nec.png'); ?>">
+    <img src="<?php $this->asset('img/footer/ninja-foto.png'); ?>">
+    <img src="<?php $this->asset('img/footer/ninja-hacker-space.png'); ?>">
+    <img src="<?php $this->asset('img/footer/poderes-pretos.png'); ?>">
+    <img src="<?php $this->asset('img/footer/som.png'); ?>">
+    <img src="<?php $this->asset('img/footer/ufa.png'); ?>">
+    <img src="<?php $this->asset('img/footer/zona-comum.png'); ?>">
+    <img src="<?php $this->asset('img/footer/zona-de-propulsao.png'); ?>">
+    <img src="<?php $this->asset('img/footer/ninja-verso.png'); ?>">
+    <img src="<?php $this->asset('img/footer/xepa.png'); ?>">
+</div>
