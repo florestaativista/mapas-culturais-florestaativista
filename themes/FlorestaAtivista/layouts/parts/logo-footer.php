9ac202144331913cfb7c72e77fa8ecba3e59eb1d
wolegario49@gmail.com <wolegario49@gmail.com>
2024-03-04 15:02:21 -0300

    Altera logotipos do rodapé

--- themes/FlorestaAtivista/layouts/parts/logo-footer.php
+++ themes/FlorestaAtivista/layouts/parts/logo-footer.php
@@ -1,29 +1,33 @@
 <?php if($app->config['module.main-footer']):?>
-<div class="logo-footer">
-    <img src="<?php $this->asset('img/footer/casa-das-redes.png'); ?>">
-    <img src="<?php $this->asset('img/footer/casa-ninja-amazonia.png'); ?>">
-    <img src="<?php $this->asset('img/footer/casa-ninja-bahia.png'); ?>">
-    <img src="<?php $this->asset('img/footer/casa-ninja-tiradentes.png'); ?>">
-    <img src="<?php $this->asset('img/footer/cine-ninja.png'); ?>">
-    <img src="<?php $this->asset('img/footer/design-ativista.png'); ?>">
-    <img src="<?php $this->asset('img/footer/ella.png'); ?>">
-    <img src="<?php $this->asset('img/footer/emergentes.png'); ?>">
-    <img src="<?php $this->asset('img/footer/esudantes-ninja.png'); ?>">
-    <img src="<?php $this->asset('img/footer/foda.png'); ?>">
-    <img src="<?php $this->asset('img/footer/fora-do-eixo.png'); ?>">
-    <img src="<?php $this->asset('img/footer/midia-ninja-1.png'); ?>">
-    <img src="<?php $this->asset('img/footer/midia-ninja-2.png'); ?>">
-    <img src="<?php $this->asset('img/footer/minas-ninja.png'); ?>">
-    <img src="<?php $this->asset('img/footer/nave.png'); ?>">
-    <img src="<?php $this->asset('img/footer/nec.png'); ?>">
-    <img src="<?php $this->asset('img/footer/ninja-foto.png'); ?>">
-    <img src="<?php $this->asset('img/footer/ninja-hacker-space.png'); ?>">
-    <img src="<?php $this->asset('img/footer/poderes-pretos.png'); ?>">
-    <img src="<?php $this->asset('img/footer/som.png'); ?>">
-    <img src="<?php $this->asset('img/footer/ufa.png'); ?>">
-    <img src="<?php $this->asset('img/footer/zona-comum.png'); ?>">
-    <img src="<?php $this->asset('img/footer/zona-de-propulsao.png'); ?>">
-    <img src="<?php $this->asset('img/footer/ninja-verso.png'); ?>">
-    <img src="<?php $this->asset('img/footer/xepa.png'); ?>">
+    <div>
+        <img src="<?php $this->asset('img/footer/1.png'); ?>">
+        <img src="<?php $this->asset('img/footer/2.png'); ?>">
+        <img src="<?php $this->asset('img/footer/3.png'); ?>">
+        <img src="<?php $this->asset('img/footer/4.png'); ?>">
+        <img src="<?php $this->asset('img/footer/5.png'); ?>">
+        <img src="<?php $this->asset('img/footer/6.png'); ?>">
+        <img src="<?php $this->asset('img/footer/7.png'); ?>">
+        <img src="<?php $this->asset('img/footer/8.png'); ?>">
+    </div>
+    <div>
+        <img src="<?php $this->asset('img/footer/9.png'); ?>">
+        <img src="<?php $this->asset('img/footer/10.png'); ?>">
+        <img src="<?php $this->asset('img/footer/11.png'); ?>">
+        <img src="<?php $this->asset('img/footer/12.png'); ?>">
+        <img src="<?php $this->asset('img/footer/13.png'); ?>">
+        <img src="<?php $this->asset('img/footer/14.png'); ?>">
+        <img src="<?php $this->asset('img/footer/15.png'); ?>">
+        <img src="<?php $this->asset('img/footer/16.png'); ?>">
+    </div>
+    <div>
+        <img src="<?php $this->asset('img/footer/17.png'); ?>">
+        <img src="<?php $this->asset('img/footer/18.png'); ?>">
+        <img src="<?php $this->asset('img/footer/19.png'); ?>">
+        <img src="<?php $this->asset('img/footer/20.png'); ?>">
+        <img src="<?php $this->asset('img/footer/21.png'); ?>">
+        <img src="<?php $this->asset('img/footer/22.png'); ?>">
+        <img src="<?php $this->asset('img/footer/23.png'); ?>">
+        <img src="<?php $this->asset('img/footer/24.png'); ?>">
+    </div>
 </div>
 <?php endif ?>
