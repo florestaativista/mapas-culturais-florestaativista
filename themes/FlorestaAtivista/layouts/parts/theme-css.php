c12f9493ae45870190ceb23b34596e676b311955
Carolina Pedrosa <carolinasouza0@gmail.com>
2024-03-26 12:10:04 -0300

    feat: modifica background do header do menu

--- themes/FlorestaAtivista/layouts/parts/theme-css.php
+++ themes/FlorestaAtivista/layouts/parts/theme-css.php
@@ -1,6 +1,6 @@
 <style>
     .main-header{
-        background-image: url(<?php $this->asset('img/barra-superior-FA.png') ?>);
+        background-image: url(<?php $this->asset('img/barra-superior-branca.png') ?>);
         background-repeat: no-repeat;
         background-position: center;
         background-size: cover;
