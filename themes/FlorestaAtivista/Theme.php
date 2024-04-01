46cf8e402221fa0ed9c2cbab8bfe4f3f14368a08
Erlei Biazzio <erleibiazzio@gmail.com>
2024-04-01 15:05:14 -0300

    Cria nova view que lista os afluentes selados

--- themes/FlorestaAtivista/Theme.php
+++ themes/FlorestaAtivista/Theme.php
@@ -18,10 +18,22 @@ class Theme extends \MapasCulturais\Themes\BaseV2\Theme {
 
         $this->enqueueStyle("app-v2", "home-logo-strip", "css/home-logo-strip.css");
 
+        // Cria endpoint para carregar as comunidades
+        $app->hook('GET(search.communities)', function() use ($app) {
+            $this->render('communities');
+        });
+
+        // Insere o menu comunidades no header
+        $app->hook('template(<<*>>.<<*>>.mc-header-menu-opportunity):after', function() {
+            $this->part('mc-header-menu-communities');
+        });
+        
+        // Insere o ícone comunidades na lista de icones padrão
         $app->hook('component(mc-icon).iconset', function(&$iconset) {
             $iconset['hand'] = 'ion:hand-right';
         });
 
+        // Insere a seção de afluentes na home
         $app->hook("component(home-entities):after", function(){
             $this->part("home-logo-strip");
         });
---
+++ themes/FlorestaAtivista/layouts/parts/mc-header-menu-communities.php
@@ -0,0 +1,21 @@
+<?php
+
+use MapasCulturais\i;
+
+/**
+ * @var MapasCulturais\App $app
+ * @var MapasCulturais\Themes\BaseV2\Theme $this
+ */
+
+?>
+
+<?php $this->applyTemplateHook('mc-header-menu-communities', 'before') ?>
+<li>
+    <?php $this->applyTemplateHook('mc-header-menu-communities', 'begin') ?>
+    <a href="<?= $app->createUrl('search', 'communities') ?>" class="mc-header-menu--item agent">
+        <span class="icon"> <mc-icon name="hand"> </span>
+        <p class="label"> <?php i::_e('Comunidades') ?> </p>
+    </a>
+    <?php $this->applyTemplateHook('mc-header-menu-communities', 'end') ?>
+</li>
+<?php $this->applyTemplateHook('mc-header-menu-communities', 'after') ?>
