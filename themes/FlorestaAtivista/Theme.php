0fc386b666d0ac4d6f4dd683c6d24994569c7930
Erlei Biazzio <erleibiazzio@gmail.com>
2024-05-22 14:45:15 -0300

    Remove rota de execução do Webmanifest

--- themes/FlorestaAtivista/Theme.php
+++ themes/FlorestaAtivista/Theme.php
@@ -55,15 +55,5 @@ class Theme extends \MapasCulturais\Themes\BaseV2\Theme {
         $app->hook('mapasculturais.body:after', function() use ($app) {
             $this->part('theme-css');
         });
-         // Manifest do five icon
-        // $app->hook('GET(site.webmanifest)', function() use ($app) {
-        //     /** @var \MapasCulturais\Controller $this */
-        //     $this->json([
-        //         'icons' => [
-        //             [ 'src' => $app->view->asset('img/favicon-192x192.png', false), 'type' => 'image/png', 'sizes' => '192x192' ],
-        //             [ 'src' => $app->view->asset('img/favicon-515x515.png', false), 'type' => 'image/png', 'sizes' => '512x512' ],
-        //         ],
-        //     ]);
-        // });
     }
 }
