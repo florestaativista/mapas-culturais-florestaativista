54868619521f3da86d91616564077c6f6affa08e
Erlei Biazzio <erleibiazzio@gmail.com>
2024-05-22 14:44:41 -0300

    Ajusta para execução de levantamento dos avatares das inscrições

--- plugins/SettingsFa/Controller.php
+++ plugins/SettingsFa/Controller.php
@@ -12,7 +12,7 @@ class Controller extends \MapasCulturais\Controllers\EntityController
 
     public function GET_noAvatarFile()
     {
-       
+
         $this->requireAuthentication();
 
         $app = App::i();
@@ -20,7 +20,7 @@ class Controller extends \MapasCulturais\Controllers\EntityController
         if (!$app->user->is('admin')) {
             return;
         }
-        
+
         $em = $app->em;
         $conn = $em->getConnection();
 
@@ -30,7 +30,8 @@ class Controller extends \MapasCulturais\Controllers\EntityController
             a.name as nome_agente,
             f.path,
             r.id as id_inscricao,	
-            o2.name as nome_da_oportunidade
+            o2.name as nome_da_oportunidade,
+            f.create_timestamp as data_criacao
         from 
             registration r 
         join 
@@ -57,11 +58,104 @@ class Controller extends \MapasCulturais\Controllers\EntityController
         foreach ($files_data as $data) {
             if (!file_exists(PUBLIC_PATH . 'files/' . $data['path'])) {
                 $result['Quantidade']++;
-                $result['ID e nome dos agentes afetados'][] = 'ID: ' . $data['id_agente'] . ' ** ' . $data['nome_agente'] . '** ---- Inscrição ' . $data['id_inscricao'] . ' ---- Oportunidade: '.$data['nome_da_oportunidade'];
+                $result['ID e nome dos agentes afetados'][] = 'ID: ' . $data['id_agente'] . ' ** ' . $data['nome_agente'] . '** ---- Inscrição ' . $data['id_inscricao'] . ' ---- Oportunidade: ' . $data['nome_da_oportunidade'] . ' ---- Data Criação: ' . $data['data_criacao'];
                 $result['data'][] = $data;
             }
         }
 
         dump($result);
+    }
+
+    public function GET_noRegistrationFile()
+    {
+
+        $this->requireAuthentication();
+
+        $app = App::i();
+
+        if (!$app->user->is('admin')) {
+            return;
+        }
+
+        $em = $app->em;
+        $conn = $em->getConnection();
+
+        $files_data = $conn->fetchAll("
+        select 
+            a.id as id_agente,
+            a.name as nome_agente,
+            f.path,
+            r2.id as id_inscricao,	
+            o.name as nome_da_oportunidade,
+            f.create_timestamp as data_criacao
+        from 
+            file f
+        join 
+            registration r2 on  f.object_id = r2.id 
+        join 
+            agent a on r2.agent_id = a.id
+        join opportunity o on r2.opportunity_id = o.id
+        where 
+            f.object_id in (
+                select 
+                    r.id 
+                from 
+                    registration r 
+                where r.status > 0
+            )
+        ");
+
+        $result['Quantidade'] = 0;
+        foreach ($files_data as $data) {
+            if (!file_exists(PRIVATE_FILES_PATH . $data['path'])) {
+                $result['Quantidade']++;
+                $result['ID e nome dos agentes afetados'][] = 'ID: ' . $data['id_agente'] . ' ** ' . $data['nome_agente'] . '** ---- Inscrição ' . $data['id_inscricao'] . ' ---- Oportunidade: ' . $data['nome_da_oportunidade'] . ' ---- Data Criação: ' . $data['data_criacao'];
+                $result['data'][] = $data;
+            }
+        }
+
+        dump($result);
+    }
+
+    public function GET_noFile()
+    {
+
+        $this->requireAuthentication();
+
+        $app = App::i();
+
+        if (!$app->user->is('admin')) {
+            return;
+        }
+
+        $em = $app->em;
+        $conn = $em->getConnection();
+
+        $files_data = $conn->fetchAll("
+        select 
+        f.path,
+        f.object_type as tipo_de_registro,
+        f.object_id as id_do_registro,
+        f.grp as grupo,
+        f.create_timestamp as data_criacao,
+        f.private
+        from 
+            file f
+        where parent_id is null
+        order by data_criacao desc
+        ");
+
+        $result['Quantidade'] = 0;
+        foreach ($files_data as $data) {
+            
+            $path = $data['private'] ? PRIVATE_FILES_PATH . $data['path'] : PUBLIC_PATH . 'files/' . $data['path'];
+            if (!file_exists($path) ) {
+                $result['Quantidade']++;
+                $result['Tipo registro'][] = 'ID: ' . $data['id_do_registro'] . ' ---- Tipo: ' . $data['tipo_de_registro'] . '----- Grupo: ' . $data['grupo'] . ' ---- Data Criação: ' . $data['data_criacao'];
+            }
+        }
+
+        dump($result);
+        dump(['data' =>  $files_data]);
     }
 }
