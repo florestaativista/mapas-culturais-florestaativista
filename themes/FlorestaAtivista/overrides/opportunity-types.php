9d96706833d99d51c31488046d99717b39470d28
Welington De Lima Olegario <wolegario49@gmail.com>
2024-06-27 09:33:49 -0300

    Adiciona rede social tiktok opportunity-types Ref.: #44

--- themes/FlorestaAtivista/overrides/opportunity-types.php
+++ themes/FlorestaAtivista/overrides/opportunity-types.php
@@ -1,4 +1,6 @@
 <?php
+
+use MapasCulturais\Utils;
 /**
  * See https://github.com/Respect/Validation to know how to write validations
  */
@@ -140,6 +142,18 @@ return array(
             'serialize' =>function($value){
                 return Utils::parseSocialMediaUser('pinterest.com', $value);
             },
+            'placeholder' => "nomedousuario",
+            'available_for_opportunities' => true
+        ),
+        'tiktok' => array(
+            'type' => "socialMedia",
+            'label' => \MapasCulturais\i::__('Tiktok'),
+            'serialize' =>function($value){
+                return Utils::parseSocialMediaUser('tiktok.com', $value);
+            },
+            'validations' => array(
+                "v::oneOf(v::urlDomain('tiktok.com'), v::regex('/^@?([-\w\d\.]+)$/i'))" => \MapasCulturais\i::__("O valor deve ser uma URL ou usuário válido.")
+            ),
             'placeholder' => "nomedousuario",
             'available_for_opportunities' => true
         ),
