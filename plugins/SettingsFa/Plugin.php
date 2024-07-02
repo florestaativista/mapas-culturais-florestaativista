6bc93dc2f0512ec3cecb1446fc9abf798b80d798
Welington De Lima Olegario <wolegario49@gmail.com>
2024-07-02 10:00:45 -0300

    Adiciona Tag Manager no plugin Ref.: #56

--- plugins/SettingsFa/Plugin.php
+++ plugins/SettingsFa/Plugin.php
@@ -9,6 +9,15 @@ class Plugin extends \MapasCulturais\Plugin
 {
     public function _init()
     {
+        $app = App::i();
+        $self = $this;
+
+        $app->hook("template(<<*>>.<<*>>.head):end", function () use($self){
+            $config = $self->config;
+            if($config['tag_manager_key']){
+            $this->part('tag-manager',["config" => $config]);
+            }
+        });
     }
 
     public function register()
