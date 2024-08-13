5c265aea1831261a383cb6f5d69d342aacab06a4
erleibiazzio <erleibiazzio@gmail.com>
2024-08-13 14:45:42 -0300

    Implementa url amigável para a oportunidade 148

--- docker/common/config.d/routes.php
+++ docker/common/config.d/routes.php
@@ -31,7 +31,8 @@ $new_routes = [
     'ATL20' => ['opportunity',25],
     'causa2024' => ['opportunity',45],
     'cinenave' => ['opportunity',47],
-    'um-dia-pela-causa' => ['opportunity',94]
+    'um-dia-pela-causa' => ['opportunity',94],
+    'somdeminas' => ['opportunity',148]
 
 ];
 foreach($new_routes as $route => $target) {
