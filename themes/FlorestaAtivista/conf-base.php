3b7eeb831f7024d470c199d75ed1870d1dd05e4a
Welington De Lima Olegario <wolegario49@gmail.com>
2025-08-21 18:38:36 -0300

    Ajusta rodapé: deixa apenas "Oportunidades" Ref.:# 69

--- themes/FlorestaAtivista/conf-base.php
+++ themes/FlorestaAtivista/conf-base.php
@@ -1,11 +1,12 @@
 <?php
-use \MapasCulturais\i;
+use MapasCulturais\i;
 
 $_config = [
     'text:registration.view.entity-address-form-nacional.public-location_adress' =>  i::__("Apareça no Mapa"),
     'text:entity-address-form-internacional.public-location-internacional' =>  i::__("Apareça no Mapa"),
     'text:agent.edit.brasil-address-form.brasil-address' => i::__("Apareça no Mapa"),
     'text:agent.*.international-address-form.international-address-forms' => i::__("Apareça no Mapa"),
+    'text:main-footer.main-footer' => i::__("oportunidades"),
 ];
 
 return $_config;
