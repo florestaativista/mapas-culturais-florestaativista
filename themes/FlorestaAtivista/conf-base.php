69fd0903a3f0dc3f7a6bcc2088f37e71ac9e16ac
Welington De Lima Olegario <wolegario49@gmail.com>
2025-08-21 18:19:24 -0300

    Adiciona configuração de texto

---
+++ themes/FlorestaAtivista/conf-base.php
@@ -0,0 +1,11 @@
+<?php
+use \MapasCulturais\i;
+
+$_config = [
+    'text:registration.view.entity-address-form-nacional.public-location_adress' =>  i::__("Apareça no Mapa"),
+    'text:entity-address-form-internacional.public-location-internacional' =>  i::__("Apareça no Mapa"),
+    'text:agent.edit.brasil-address-form.brasil-address' => i::__("Apareça no Mapa"),
+    'text:agent.*.international-address-form.international-address-forms' => i::__("Apareça no Mapa"),
+];
+
+return $_config;
