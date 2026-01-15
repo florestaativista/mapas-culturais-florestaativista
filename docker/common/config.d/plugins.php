d0dad8cb8678064a1346f74258cef023243db336
erleibiazzio <erleibiazzio@gmail.com>
2026-01-15 13:16:00 -0300

    Habilita o zammad para trabalhar com organizações e controla o chat ID

--- docker/common/config.d/plugins.php
+++ docker/common/config.d/plugins.php
@@ -18,5 +18,15 @@ return [
         'SpamDetector',
         'PluginSOM',
         //'LocationPatch'
+        'Zammad' => [
+            'namespace' => 'Zammad',
+            'config' => [
+                'enabled' => true,
+                'url' => 'https://suporte.florestaativista.org/assets/chat/chat-no-jquery.min.js',    
+                'background' => '#F66968',
+                'title' => 'Duvidas? Fale conosco',
+                'chatId' => 2,
+            ]
+        ],
     ]
 ];
