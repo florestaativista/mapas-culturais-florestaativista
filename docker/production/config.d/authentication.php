fa4aa0fde16c83378480e2b5b7ec27e77b5f4929
erleibiazzio <erleibiazzio@gmail.com>
2026-03-06 16:11:41 -0300

    Habilita o plugin MultipleLocalAUth no Ambiente de DEV

--- docker/production/config.d/authentication.php
+++ docker/production/config.d/authentication.php
@@ -3,19 +3,38 @@
 $prot_part = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] ? 'https://' : 'http://';
 //added @ for HTTP_HOST undefined in Tests
 $host_part = @$_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']);
-if (substr($host_part, -1) !== '/') $host_part .= '/';
+if(substr($host_part,-1) !== '/') $host_part .= '/';
 $_APP_BASE_URL = $prot_part . $host_part;
 
 return [
-    'auth.provider' => 'MapasCulturais\AuthProviders\OpauthAuthentik',
-    'auth.config' => [
-        'salt' => env('AUTH_SALT', 'SECURITY_SALT'),
+    'auth.provider' => '\MultipleLocalAuth\Provider',
+    'auth.config' => array(
+        'salt' => env('AUTH_SALT', null),
         'timeout' => '24 hours',
-        'client_id' => env('AUTH_AUTHENTIK_APP_ID', ''),
-        'client_secret' => env('AUTH_AUTHENTIK_APP_SECRET', ''),
-        'scope' => env('AUTH_AUTHENTIK_SCOPE', ''),
-        'login_url' => env('AUTH_AUTHENTIK_LOGIN_URL', ''),
-        'logout_url' => env('AUTH_AUTHENTIK_LOGOUT_URL', ''),
-        'change_password_url' => env('AUTH_AUTHENTIK_CHANGE_PASSWORD_URL', null),
-    ]
+        'strategies' => [
+            'Facebook' => array(
+               'app_id' => env('AUTH_FACEBOOK_APP_ID', null),
+               'app_secret' => env('AUTH_FACEBOOK_APP_SECRET', null),
+               'scope' => env('AUTH_FACEBOOK_SCOPE', 'email'),
+            ),
+
+            'LinkedIn' => array(
+                'api_key' => env('AUTH_LINKEDIN_API_KEY', null),
+                'secret_key' => env('AUTH_LINKEDIN_SECRET_KEY', null),
+                'redirect_uri' => $_APP_BASE_URL . 'autenticacao/linkedin/oauth2callback',
+                'scope' => env('AUTH_LINKEDIN_SCOPE', 'r_emailaddress')
+            ),
+            'Google' => array(
+                'client_id' => env('AUTH_GOOGLE_CLIENT_ID', null),
+                'client_secret' => env('AUTH_GOOGLE_CLIENT_SECRET', null),
+                'redirect_uri' => $_APP_BASE_URL . 'autenticacao/google/oauth2callback',
+                'scope' => env('AUTH_GOOGLE_SCOPE', 'email'),
+            ),
+            'Twitter' => array(
+                'app_id' => env('AUTH_TWITTER_APP_ID', null),
+                'app_secret' => env('AUTH_TWITTER_APP_SECRET', null),
+            ),
+
+        ]
+    ),
 ];
