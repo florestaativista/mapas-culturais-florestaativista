707b9e203f5833534696a88ef6f996db2feaa128
erleibiazzio <erleibiazzio@gmail.com>
2026-08-07 14:31:14 -0300

    feat(florestaativista): adiciona catálogo de tradução es_ES/en_US ao tema

--- themes/FlorestaAtivista/Theme.php
+++ themes/FlorestaAtivista/Theme.php
@@ -2,17 +2,20 @@
 namespace FlorestaAtivista;
 
 use MapasCulturais\App;
+use MapasCulturais\i;
 
 
 // class Theme extends \Subsite\Theme {
 class Theme extends \MapasCulturais\Themes\BaseV2\Theme {
- 
+
     static function getThemeFolder() {
         return __DIR__;
     }
 
     function _init() {
         parent::_init();
+
+        i::load_textdomain('default', __DIR__ . '/translations', i::get_locale());
 
         $app = App::i();
 
